db8953335718b04f75f4ef2abfe6f421d87be489
Mahbubur Rahman <rifat0153@gmail.com>
2025-02-10 19:40:45 +0000

    Update launch configuration for .NET 9.0 and modify task arguments; add exit prompt in Program.cs

--- Program.cs
+++ Program.cs
@@ -5,3 +5,10 @@ using Leetcode.interview;
 using Leetcode.Medium;
 
 Permutation.Test();
+
+while (true)
+{
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadKey();
+    break;
+}
