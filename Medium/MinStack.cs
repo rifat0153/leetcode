2e355111c3c16520d5b88c483a1c0434134ab240
Mahbubur Rahman <rifat0153@gmail.com>
2025-02-14 22:16:23 +0000

    Add MinStack class with stack operations and fix initialization
    Introduce MinStack class in Leetcode.Medium namespace. Implement
    Push, Pop, Top, and GetMin methods using a stack of tuples to
    track values and minimums. Correct stack initialization from
    [] to new Stack<(int Val, int Min)>().

---
+++ Medium/MinStack.cs
@@ -0,0 +1,37 @@
+﻿namespace Leetcode.Medium;
+
+class MinStack
+{
+    private Stack<(int Val, int Min)> stack = [];
+
+    public MinStack() { }
+
+    public void Push(int val)
+    {
+        if (stack.TryPeek(out var p))
+        {
+            stack.Push((val, Math.Min(val, p.Min)));
+        }
+        else
+        {
+            stack.Push((val, val));
+        }
+    }
+
+    public void Pop()
+    {
+        stack.TryPop(out var _);
+    }
+
+    public int Top()
+    {
+        stack.TryPeek(out var v);
+        return v.Val;
+    }
+
+    public int GetMin()
+    {
+        stack.TryPeek(out var v);
+        return v.Min;
+    }
+}
