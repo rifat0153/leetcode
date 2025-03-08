e2c73b2aeeef45891f957132afa04dd3628ba0e6
Mahbubur Rahman <rifat0153@gmail.com>
2025-03-08 23:56:29 +0000

    Add CarFleet class with fleet calculation method
    Introduced a new namespace `Leetcode.Medium` and added an internal class `CarFleet`. Implemented the `CarFleetTest` method to calculate the number of car fleets arriving at the destination using a stack. Added a `Test` method to demonstrate the functionality with sample data.

---
+++ Medium/CarFleet.cs
@@ -0,0 +1,46 @@
+﻿using LanguageExt.TypeClasses;
+
+namespace Leetcode.Medium;
+
+internal class CarFleet
+{
+    public static void Test()
+    {
+        int target = 12;
+        int[] position = [10, 8, 0, 5, 3];
+        int[] speed = [2, 4, 1, 1, 3];
+
+        Console.WriteLine(CarFleetTest(target, position, speed));
+    }
+
+    public static int CarFleetTest(int target, int[] position, int[] speed)
+    {
+        if (position.Length == 0)
+            return 0;
+
+        Stack<(int pos, int speed)> stack = new();
+
+        var pairs = position.Zip(speed, (p, s) => (p, s)).OrderByDescending(x => x.p);
+
+        foreach (var pair in pairs)
+        {
+            if (!stack.TryPeek(out var prevPair))
+            {
+                stack.Push(pair);
+                continue;
+            }
+
+            stack.Push(pair);
+
+            var prevTime = (double)(target - prevPair.pos) / prevPair.speed;
+            var currTime = (double)(target - pair.p) / pair.s;
+
+            if (currTime <= prevTime)
+            {
+                stack.Pop();
+            }
+        }
+
+        return stack.Count;
+    }
+}
