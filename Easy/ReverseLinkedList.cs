91801e18762ad482ad6ae352d8bb62b9525a0384
Mahbubur Rahman <rifat0153@gmail.com>
2025-03-17 22:07:56 +0000

    Add ReverseLinkedList implementation and update Program.cs
    - Removed MinimumWindowSubstring.Test() call from Program.cs
    - Added ReverseLinkedList.Test() call to Program.cs
    - Added using directive for Leetcode.interview in ReverseLinkedList.cs
    - Introduced Leetcode.Easy namespace in ReverseLinkedList.cs
    - Defined ListNode record for linked list nodes
    - Added internal ReverseLinkedList class with:
      - Test method to create, reverse, and print a linked list
      - ReverseList method to reverse a linked list

---
+++ Easy/ReverseLinkedList.cs
@@ -0,0 +1,44 @@
+﻿using Leetcode.interview;
+
+namespace Leetcode.Easy;
+
+public record ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+internal class ReverseLinkedList
+{
+    public static void Test()
+    {
+        ListNode listNode = new(val: 1, next: new(2, new(2, new(3, new(4, new(5, null))))));
+        ListNode reversed = ReverseList(listNode);
+
+        while (reversed != null)
+        {
+            Console.WriteLine(reversed.val);
+            reversed = reversed.next;
+        }
+    }
+
+    internal static ListNode ReverseList(ListNode head)
+    {
+        ListNode? prev = null;
+        while (head is not null)
+        {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Leetcode.interview;
 using Leetcode.Medium;
 using Leetcode.Sorting;
 
-MinimumWindowSubstring.Test();
+ReverseLinkedList.Test();
 
 while (true)
 {
